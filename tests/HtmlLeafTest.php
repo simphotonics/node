6d5cc20a8f24ef9f0ae460701b38320d4544fb93
D Reschner <git@simphotonics.com>
2016-02-18 19:03:23 +0000

    Initial commit.

---
+++ tests/HtmlLeafTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Simphotonics\Dom\Tests;
+
+use Simphotonics\Dom\HtmlLeaf;
+
+/**
+ * @author D Reschner <d.reschner@simphotonics.com>
+ * @copyright 2015 Simphotonics
+ * Description: Tests Simphotonics\HtmlNode methods.
+ */
+class HtmlLeafTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRegisterElement()
+    {
+        $element = ['br'=>'renderEmpty'];
+        HtmlLeaf::registerElements($element);
+        $this->assertArraySubset($element, HtmlLeaf::getElements());
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testRegisterElementException()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        HtmlLeaf::registerElements(['img' => 'nonExistingMethod']);
+    }
+
+    public function testRenderBlock()
+    {
+        $l = new HtmlLeaf(['attr'=> ['class'=> 'main bold'],
+            'cont'=>'Test Leaf!']);
+        $this->assertEquals(
+            '<div class="main bold">Test Leaf!</div>',
+            "$l"
+        );
+    }
+
+    public function testRenderEmpty()
+    {
+        HtmlLeaf::registerElements(['br'=>'renderEmpty']);
+        $l = new HtmlLeaf(['kind' => 'br']);
+        $this->assertEquals('<br />', "$l");
+    }
+   
+    public function testRenderComment()
+    {
+        $l = new HtmlLeaf(['kind'=> '!--','cont' => 'This is a comment!']);
+        $this->assertEquals('<!-- This is a comment! -->', "$l");
+    }
+
+    public function testRenderDTD()
+    {
+        HtmlLeaf::setDTD('html5');
+        $dtd = new HtmlLeaf(['kind' => 'DOCTYPE!']);
+        $this->assertEquals('<DOCTYPE! html5 >', "$dtd");
+        $dtd->setCont('html5');
+        $this->assertEquals('<DOCTYPE! html5 >', "$dtd");
+    }
+}
