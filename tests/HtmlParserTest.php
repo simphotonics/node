c5875841dd2cb9d107d22a34d48a4a273f43fa53
D Reschner <git@simphotonics.com>
2016-03-16 14:33:26 +0000

    Namespace of HtmlParser changed!

--- tests/HtmlParserTest.php
+++ tests/HtmlParserTest.php
@@ -4,7 +4,7 @@ namespace Simphotonics\Dom\Tests;
 
 use Simphotonics\Dom\HtmlLeaf;
 use Simphotonics\Dom\HtmlNode;
-use Simphotonics\Dom\HtmlParser;
+use Simphotonics\Dom\Parser\HtmlParser;
 
 /**
  * @author D Reschner <d.reschner@simphotonics.com>
